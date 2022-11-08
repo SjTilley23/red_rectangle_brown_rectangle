b6fe3caeaf1e7a5f346efb5b7023ac6fe87c0d68
Samjohn03 <sj.tilley23@gmail.com>
2022-11-08 02:05:52 -0500

    update

--- fonts_rendering.py
+++ fonts_rendering.py
@@ -1,3 +1,4 @@
 class Fonts():
     def __init__(self) -> None:
-        self.tahoma20 = pygame.font.Sys_font("Tahoma",20)
+        self.tahoma20 = pygame.font.SysFont("Tahoma",20)
+        
--- rrbr_main.py
+++ rrbr_main.py
@@ -1,3 +1,31 @@
 import pygame
 pygame.init
 
+window = pygame.display.set_mode([800,800])
+GAME_GO_BRRR = True
+
+
+while GAME_GO_BRRR:
+
+    #Quitting
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            GAME_GO_BRRR = False
+
+    #window
+    window.fill((150,150,150))
+
+
+
+
+
+
+
+
+
+
+
+
+
+    pygame.display.flip()
+pygame.quit()
